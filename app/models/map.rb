89cdaf9259eb9e738b5225e545c40f4afa1908cd
完田朋美 <b230163@hiroshima-u.ac.jp>
2025-09-03 09:48:18 +0900

    住所の日本語表示と表示順番整理

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,9 @@
 class Map < ApplicationRecord
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
+
+  def display_address
+    address_array = address.split(",")
+    "#{address_array[-1]} #{address_array[-3]} #{address_array[-4]} #{address_array[-5]}"
+  end
 end
